6bd2ecc5d443e2605b9b1fd49cdfafa2c32fadbc
John Wyss <jwyss@lockton.com>
2018-09-17 14:21:51 -0500

    test

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// this file isnt transpiled so must use common js and es5
+
+// register babel to transpile before our tests run
+require('babel-register')();
+
+// disable webpack features that mocha doesn't understand
+require.extensions['.css'] = function() {};
